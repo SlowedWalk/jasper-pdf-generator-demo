7a9a6bcd4d0c0d93f51f32ddef7cd9d6fdbc3627
SlowedWalk <idetoratojo1@gmail.com>
2023-10-24 08:45:21 +0100

    *

--- src/main/java/tech/hidetora/pdfinvoice/controller/InvoiceController.java
+++ src/main/java/tech/hidetora/pdfinvoice/controller/InvoiceController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tech.hidetora.pdfinvoice.entity.User;
-import tech.hidetora.pdfinvoice.repository.UserRepository;
 import tech.hidetora.pdfinvoice.service.UserService;
 
 import java.io.FileInputStream;
